c1ce3be88ad802a1218d7f838b951f0e2d96abb0
JoseGzz <josegzza@gmail.com>
2017-03-23 13:53:28 -0600

    Agrega leyendas de colores para identificación de tarea. Falta identificación de máquinas.

--- JSSP/schedule.py
+++ JSSP/schedule.py
@@ -13,9 +13,7 @@ class Schedule:
         pass
 
     def fit(self, graph, no_machines, machines, operations, ms, no_jobs):
-        #machines_location = list(range(no_machines))
-        #print(machines_location)	
-
+	
         '''
 		machines_lst = [None]*no_machines
 
@@ -26,28 +24,42 @@ class Schedule:
                 if op.get_machine_id() == m.get_id():
                     machines_tmp.append(op)
             machines_lst[i] = machines_tmp
-	
-		
         '''
-        # pr = [patches.rectangle,...]
-        max_value = 16581375 #255**3
+
+        max_value = 16581375 
         interval = int(max_value / no_jobs)
         colors = [hex(I)[2:].zfill(6) for I in range(0, max_value, interval)]
 
         patch_list = []
+        job_colors = [None]*no_jobs
         for k, v in graph.items():
             y = v[0].get_machine_id()
             x = v[0].get_start_time()
             coordinates = (x,y)
             color = colors[v[0].get_job_id()]
+            job_colors[v[0].get_job_id()] = color
             patch_list.append(patches.Rectangle(coordinates, v[0].get_duration(), 1, facecolor='#'+str(color)))
 
+
+        color_patches = []
+        labels = []
+        for i, j_color in enumerate(job_colors):
+            labels.append('Job #' + str(i+1))
+            color_patch = patches.Patch(color="#"+str(j_color), label='Job #' + str(i))
+            color_patches.append(color_patch)
+
+        #textstr = 'NSE=%.2f\nRMSE=%.2f\n'%(1, 2)
+        #plt.text(5, 5, textstr, fontsize=14)
+
         fig = plt.figure()
-        ax = fig.add_subplot(111, aspect='equal', label='?')
+        fig.legend(labels=labels, handles=color_patches)
+        ax = fig.add_subplot(111, aspect='equal', label='hello')
         ax.set_xlim(xmin=0,xmax=int(float(ms))+5)
         ax.set_ylim(ymin=0,ymax=int(float(no_machines+3)))
+
         for p in patch_list:
             ax.add_patch(p)        
+
         plt.show()
 
 
